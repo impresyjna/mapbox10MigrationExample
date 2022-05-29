dfd76d902e4d3a4e907ead2cb74282b35b0aa0a8
Asia Kasprzycka <impresyjna@gmail.com>
2022-05-29 16:28:32 +0200

    Initialize mapViews

--- mapbox10MigrationExample/ViewController.swift
+++ mapbox10MigrationExample/ViewController.swift
@@ -13,7 +13,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func openNewMapboxView(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "NewMapViewController")
+        self.present(vc, animated: true)
+    }
+    
+    @IBAction func openOldMapboxView(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "OldMapViewController")
+        self.present(vc, animated: true)
+    }
 }
 
