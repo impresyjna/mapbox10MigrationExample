2d178dde85f48b1d0a110a4826b5b5750491f8be
Asia Kasprzycka <impresyjna@gmail.com>
2022-05-29 17:32:07 +0200

    Initialize NewMapView

--- mapbox10MigrationExample/NewMapViewController.swift
+++ mapbox10MigrationExample/NewMapViewController.swift
@@ -7,7 +7,22 @@
 
 import Foundation
 import UIKit
+import MapboxMaps
 
 class NewMapViewController: UIViewController {
+    var mapView: MapView?
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        initializeMapView()
+    }
+    
+    func initializeMapView() {
+        guard mapView == nil else { return }
+        
+        mapView = MapView(frame: view.bounds)
+        guard let mapView = mapView else { return }
+        
+        view.addSubview(mapView)
+    }
 }
